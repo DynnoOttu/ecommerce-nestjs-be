b9ce9b0c3a37e3224a01abd54ef9d7c252322fec
Dynno Yohanis Ottu <dynnoottu968@gmail.com>
2026-02-21 15:10:17 +0700

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './modules/product/product.module';
+import { PrismaModule } from './infra/database/prisma.module';
+//import { AuthModule } from '@thallesp/nestjs-better-auth';
+// import { auth } from './infra/auth/auth';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // AuthModule.forRoot({
+    //   auth: auth,
+    // }),
+    ProductModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
